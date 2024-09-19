61124010e759def3200fca0e6d24859c7c4d767b
Aniruddha Chitte <chitte.a@northeastern.edu>
2024-09-19 18:06:02 -0400

    Assignment 1 commit

---
+++ Assignment_1/src/model/Profile.java
@@ -0,0 +1,195 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Aniruddha
+ */
+public class Profile {
+    
+    String first_name;
+    String last_name;
+    String email;
+    String phone;
+    String address_line_1;
+    String address_line_2;
+    String city;
+    String state;
+    String zip;
+    String country;
+    String date_of_birth;
+    String sex;
+    String veteran_status;
+    String telephone_number;
+    String passport_number;
+    String fax_number;
+    String social_security_number;
+    String drivers_license_state;
+    String drivers_license_number;
+    String county;
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress_line_1() {
+        return address_line_1;
+    }
+
+    public void setAddress_line_1(String address_line_1) {
+        this.address_line_1 = address_line_1;
+    }
+
+    public String getAddress_line_2() {
+        return address_line_2;
+    }
+
+    public void setAddress_line_2(String address_line_2) {
+        this.address_line_2 = address_line_2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getDate_of_birth() {
+        return date_of_birth;
+    }
+
+    public void setDate_of_birth(String date_of_birth) {
+        this.date_of_birth = date_of_birth;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getVeteran_status() {
+        return veteran_status;
+    }
+
+    public void setVeteran_status(String veteran_status) {
+        this.veteran_status = veteran_status;
+    }
+
+    public String getTelephone_number() {
+        return telephone_number;
+    }
+
+    public void setTelephone_number(String telephone_number) {
+        this.telephone_number = telephone_number;
+    }
+
+    public String getPassport_number() {
+        return passport_number;
+    }
+
+    public void setPassport_number(String passport_number) {
+        this.passport_number = passport_number;
+    }
+
+    public String getFax_number() {
+        return fax_number;
+    }
+
+    public void setFax_number(String fax_number) {
+        this.fax_number = fax_number;
+    }
+
+    public String getSocial_security_number() {
+        return social_security_number;
+    }
+
+    public void setSocial_security_number(String social_security_number) {
+        this.social_security_number = social_security_number;
+    }
+
+    public String getDrivers_license_state() {
+        return drivers_license_state;
+    }
+
+    public void setDrivers_license_state(String drivers_license_state) {
+        this.drivers_license_state = drivers_license_state;
+    }
+
+    public String getDrivers_license_number() {
+        return drivers_license_number;
+    }
+
+    public void setDrivers_license_number(String drivers_license_number) {
+        this.drivers_license_number = drivers_license_number;
+    }
+
+    public String getCounty() {
+        return county;
+    }
+
+    public void setCounty(String county) {
+        this.county = county;
+    }
+
+    
+}
